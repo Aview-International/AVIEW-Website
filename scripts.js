4a8841f7bd7a8c1f50a652680da9f641cf8556f2
Andrew Qiao <andrewqiao2004@gmail.com>
2022-05-18 22:39:26 -0400

    Fix viewport height bug on mobile

--- scripts.js
+++ scripts.js
@@ -2,6 +2,11 @@ const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const navbar = document.getElementById("navbar");
 
+function setViewportHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
 function toggleMenu() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -22,4 +27,5 @@ window.addEventListener("resize", () => {
   document.body.classList.remove("no-scroll");
   navbar.classList.remove("navbar-open");
   closeBtn.classList.remove("close-btn-open");
+  setViewportHeight();
 });
